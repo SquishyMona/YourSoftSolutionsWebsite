b67290e55b6373c311543247c09e3a0973e0548d
SquishyMona <kotonefemc@gmail.com>
2025-01-28 14:28:51 -0500

    first commit

---
+++ your-soft-solutions-website/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+export default function Navbar() {
+    return (
+        <nav className="m-10 flex text-xl">
+            <Image src="/logo.png" alt="Your Soft Solutions, LLC" width={75} height={75} />
+            <a className="mr-auto ml-2 self-center" href="#">Your Soft Solutions, LLC</a>
+            <ul className="flex flex-row">
+                <li className="self-center mx-2">
+                <a href="#">Home</a>
+                </li>
+                <li className="self-center mx-2">
+                <a href="#">Services</a>
+                </li>
+                <li className="self-center mx-2">
+                <a className='rounded-full bg-[#4FADEB] p-5 text-white' href="#">Contact Us</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
