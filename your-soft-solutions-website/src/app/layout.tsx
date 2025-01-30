a691295f28b3f772f912eaa75427c4be39c3dbfc
SquishyMona <kotonefemc@gmail.com>
2025-01-30 12:34:57 -0500

    added new font

--- your-soft-solutions-website/src/app/layout.tsx
+++ your-soft-solutions-website/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
@@ -12,6 +13,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const aptos = localFont({
+  src: './Aptos.ttf',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${aptos.className} antialiased`}
       >
         <Navbar />
         {children}
