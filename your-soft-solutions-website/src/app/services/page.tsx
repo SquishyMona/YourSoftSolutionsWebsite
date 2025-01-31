0ca4168f18585b5bc91edd97a5b33468bf8c4915
SquishyMona <kotonefemc@gmail.com>
2025-01-31 13:45:01 -0500

    responsive navbar

--- your-soft-solutions-website/src/app/services/page.tsx
+++ your-soft-solutions-website/src/app/services/page.tsx
@@ -6,25 +6,25 @@ export default function Services() {
             <div className="flex flex-col text-black text-center my-36 mx-20 md:mx-36">
                 <div className="flex flex-col items-center justify-center">
                     <h1 className="text-4xl font-bold p-3">Services</h1>
-                    <p className="text-2xl p-5">We provide a wide range of services, such as web services, application development, and digital marketing.</p>
+                    <p className="text-2xl p-5">YSS provides a wide range of services, such as web services, application development, and digital marketing.</p>
                 </div>
             </div>
             <div className="py-40 bg-[#4FADEB90]">
                 <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black'>
                     <h2 className="text-3xl font-bold p-3">Web Services</h2>
-                    <p className="text-xl p-5">We provide web services, such as web design and web application development.</p>
+                    <p className="text-xl p-5">YSS provides web services, such as web design and web application development.</p>
                 </div>
             </div>
             <div className="py-40 bg-[#E0773190]">
                 <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black'>
                     <h2 className="text-3xl font-bold p-3">Application Development</h2>
-                    <p className="text-xl p-5">We provide application development services, such as mobile app development and desktop app development.</p>
+                    <p className="text-xl p-5">YSS provides application development services, such as mobile app development and desktop app development.</p>
                 </div>
             </div>
             <div className="py-40 bg-[#4C7B3190]">
                 <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black'>
                     <h2 className="text-3xl font-bold p-3">Digital Marketing</h2>
-                    <p className="text-xl p-5">We provide digital marketing services, such as search engine optimization (SEO) and social media marketing.</p>
+                    <p className="text-xl p-5">YSS provides digital marketing services, such as search engine optimization &#40;SEO&#41; and social media marketing.</p>
                 </div>
             </div>
         </div>
