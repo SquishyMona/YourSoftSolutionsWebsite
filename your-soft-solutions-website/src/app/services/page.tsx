04c7e7d745f1cea659036abcecfaf9b6d42eb3ef
SquishyMona <kotonefemc@gmail.com>
2025-01-31 13:55:56 -0500

    added more detail in the services section

--- your-soft-solutions-website/src/app/services/page.tsx
+++ your-soft-solutions-website/src/app/services/page.tsx
@@ -6,25 +6,25 @@ export default function Services() {
             <div className="flex flex-col text-black text-center my-36 mx-20 md:mx-36">
                 <div className="flex flex-col items-center justify-center">
                     <h1 className="text-4xl font-bold p-3">Services</h1>
-                    <p className="text-2xl p-5">YSS provides a wide range of services, such as web services, application development, and digital marketing.</p>
+                    <p className="text-2xl p-5">YSS provides a wide range of services, such as web services, application development, and digital marketing. All our services can be tailored to your specific needs</p>
                 </div>
             </div>
             <div className="py-40 bg-[#4FADEB90]">
-                <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black'>
+                <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black text-center'>
                     <h2 className="text-3xl font-bold p-3">Web Services</h2>
-                    <p className="text-xl p-5">YSS provides web services, such as web design and web application development.</p>
+                    <p className="text-xl p-5">YSS specializes in web design and web application development, creating visually appealing and highly functional websites tailored to your business requirements. YSS also offers solutions designed to appeal to the general public.</p>
                 </div>
             </div>
             <div className="py-40 bg-[#E0773190]">
-                <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black'>
-                    <h2 className="text-3xl font-bold p-3">Application Development</h2>
-                    <p className="text-xl p-5">YSS provides application development services, such as mobile app development and desktop app development.</p>
+                <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black text-center'>
+                    <h2 className="text-3xl font-bold p-3">Application Development and Management</h2>
+                    <p className="text-xl p-5">YSS offers top-notch mobile and desktop application development services, ensuring your applications are user-friendly, efficient, and scalable. YSS also offers software management solutions, providing our clients with special tooling to assist them in managing their software across many different sectors..</p>
                 </div>
             </div>
             <div className="py-40 bg-[#4C7B3190]">
-                <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black'>
+                <div className='mx-20 md:mx-36 flex flex-col items-center justify-center text-black text-center'>
                     <h2 className="text-3xl font-bold p-3">Digital Marketing</h2>
-                    <p className="text-xl p-5">YSS provides digital marketing services, such as search engine optimization &#40;SEO&#41; and social media marketing.</p>
+                    <p className="text-xl p-5">YSS provides digital marketing services, such as search engine optimization &#40;SEO&#41; and social media marketing, to help our clients reach a wider audience and grow their online presence. </p>
                 </div>
             </div>
         </div>
