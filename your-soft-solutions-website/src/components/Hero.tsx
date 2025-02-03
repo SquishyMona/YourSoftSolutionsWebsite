b51107bae0662727484f00861b04bf5f58477c13
Ian Cioppa <ian.cioppa@gmail.com>
2025-02-03 14:03:56 -0500

    added some images

--- your-soft-solutions-website/src/components/Hero.tsx
+++ your-soft-solutions-website/src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
             <div className="flex flex-col items-center justify-center md:w-1/2">
                 <h1 className="text-4xl font-bold p-3">Your Soft Solutions</h1>
                 <p className="text-2xl p-5">We are a software development company that specializes in web applications and services and digital marketing.</p>
-                <button className="rounded-full bg-white/70 px-5 py-3 m-3 text-black">Learn More</button>
+                { /* <button className="rounded-full bg-white/70 px-5 py-3 m-3 text-black">Learn More</button> */ }
             </div>
             <div className="flex flex-col items-center justify-center my-5 md:w-1/2">
                 <Image src="/logonotext.png" alt="Your Soft Solutions, LLC Logo" width={400} height={400} />
